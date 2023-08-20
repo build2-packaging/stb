aa0153b9ec6a3b86a7ff476f9c21f544394b35fa
lyrahgames <lyrahgames@mailbox.org>
2023-08-20 20:28:52 +0200

    Fix implicit function declaration for 'stb_dxt'

--- stb_dxt/tests/basics/main.c
+++ stb_dxt/tests/basics/main.c
@@ -1,3 +1,10 @@
+// The implementation uses 'memcpy' which,
+// unfortunately, is not declared explicitly before.
+// We add this header to remove warnings and compile
+// errors on implicit function declarations.
+//
+#include <string.h>
+
 #define STB_DXT_IMPLEMENTATION
 #include <stb_dxt.h>
 
